a45bb14d986a6044da0390502ffc090dc2b80f86
hanilcome <hanilcome@naver.com>
2023-05-23 09:04:02 +0900

    :lipstick: project base setting+

--- articles/urls.py
+++ articles/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('<int:article_id>/', views.ArticleDetailView.as_view(), name='article_detail_view'),
     path('<int:article_id>/likes/', views.ArticleLikesView.as_view(), name='article_likes_view'),
     path('<int:article_id>/comments/', views.CommentsView.as_view(), name='comment_view'),
-    path('comments/<int:comment_id>/', views.CommentsDetailView.as_view(), name='comments_detail_view'),
+    path('<int:article_id>/comments/<int:comment_id>/', views.CommentsDetailView.as_view(), name='comments_detail_view'),
 ]
