8979c5ff07e342f10164f63a02d42906eb3d0368
VinylStage <mcpe9869@gmail.com>
2023-05-25 17:12:16 +0900

    :umbrella:Test: test코드 추가 및 일부 수정
    23.05.13
    IOTD/tests/test_articles.py : 아티클 테스트코드추가
    IOTD/test/test_users.py : 유저 테스트코드 테스트

--- IOTD/tests/test_articles.py
+++ IOTD/tests/test_articles.py
@@ -1,38 +1,43 @@
-# import os
+import os
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IOTD.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "IOTD.settings")
 
-# from django.core.wsgi import get_wsgi_application
-# application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
 
-# from users.models import User
-# from articles.models import Article
-# from rest_framework.test import APITestCase
-# from django.urls import reverse
-# from rest_framework import status
+application = get_wsgi_application()
 
-# class PostingViewTest(APITestCase):
-#     @classmethod
-#     def setUpTestData(cls):
-#         cls.user_data = {"email": "test@test.com", "password": "test"}
-#         cls.posting_data = {"title": "test", "content": "test content"}
-#         cls.user = User.objects.create_user("test@test.com", "test")
-        
-#     def setUp(self):
-#         self.access_token = self.client.post(reverse("token_obtain_pair"), self.user_data).data["access"]
-    
-#     def tearDown(self):
-#         for articles in Article.objects.all():
-#             articles.image.delete()
-#             articles.delete()
-            
-    
-#     def test_create_posting_success(self):
-#         response = self.client.post(
-#             path=reverse("posting_view"),
-#             data=self.posting_data,
-#             HTTP_AUTHORIZATION=f"Bearer {self.access_token}",
-#         )
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-#         self.assertEqual(Article.objects.count(), 1)
-#         self.assertEqual(Article.objects.get().title, "test Title")
+from users.models import User
+from articles.models import Article
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+class PostingViewTest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        response = APIClient().post(
+            "/users/signup/",
+            {
+                "email": "test@test.com",
+                "password": "test",
+            },
+            format="json",
+        )
+        return response
+
+    def tearDown(self):
+        for articles in Article.objects.all():
+            articles.image.delete()
+            articles.delete()
+
+    # def test_create_posting_success(self):
+    #     response = self.client.post(
+    #         path=reverse("posting_view"),
+    #         data=self.posting_data,
+    #         HTTP_AUTHORIZATION=f"Bearer {self.access_token}",
+    #     )
+    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    #     self.assertEqual(Article.objects.count(), 1)
+    #     self.assertEqual(Article.objects.get().title, "test Title")
--- IOTD/tests/test_users.py
+++ IOTD/tests/test_users.py
@@ -1,8 +1,9 @@
 # import os
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IOTD.settings')
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "IOTD.settings")
 
 # from django.core.wsgi import get_wsgi_application
+
 # application = get_wsgi_application()
 
 # from users.models import User
@@ -10,24 +11,17 @@
 # from rest_framework.test import APITestCase
 # from rest_framework import status
 # import pytest
+# from rest_framework.test import APIClient
+
+
 # @pytest.mark.django_db
-# class LoginUserTest(APITestCase):
-#     def setUp(self):
-
-#         self.data = {'username': 'john', 'password': 'test'}
-#         self.user = User.objects.create_user('john', 'test')
-
-#     def test_login(self):
-#         response = self.client.post(reverse('token_obtain_pair'), self.data)
-        
-#         self.assertEqual(response.status_code, 200)
-        
-#     def test_get_user_data(self):
-#         access_token = self.client.post(reverse('token_obtain_pair'), self.data).data['access']
-#         print(access_token)
-#         response = self.client.get(
-#             path=reverse("user_view"),
-#             HTTP_AUTHORIZATION=f"Bearer {access_token}"
-#         )
-#         self.assertEqual(response.status_code, 200)
-
+# def test_sign_up():
+# response = APIClient().post(
+#     "/users/signup/",
+#     {
+#         "email": "test@test.com",
+#         "password": "test",
+#     },
+#     format="json",
+# )
+#     assert response.status_code == 201
